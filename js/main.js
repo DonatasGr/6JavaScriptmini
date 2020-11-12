ac6ea8f375f8772aecf66fd8cb73fba9bcb50558
DonatasGr <donatas888@gmail.com>
2020-11-12 09:47:05 +0200

    6JavaScripHomeW

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+// Kintamuju inicijavimas
+
+
+// 1.Sukurti 3 kintamuosius su skaiciaus tipo reiksmemis
+
+const n1 = 33;
+console.log(n1);
+
+const n2 = 8.88;
+console.log(n2);
+
+const n3 = -19;
+console.log(n3);
+
+
+// 2.Sukurti 3 kintamuosius su teksto tipo reiksmemis
+// Mama, Tetis ir Vaikas
+
+const text1 = 'Mama';
+const text2 = 'Tetis';
+const text3 = 'Vaikas';
+const Seima = text1 + ',' + ' ' + text2 + " " + 'ir'+ ' ' + text3;
+
+console.log(Seima);
+
